e04a2d17bf2efeae3d5561557af2f19521926aa5
zxzx93 <thddustn26@hanmail.net>
2022-09-06 01:39:27 +0900

    context provider custom hook 작성

---
+++ src/contexts/OrderDetails.jsx
@@ -0,0 +1,83 @@
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useMemo,
+  createContext,
+} from "react";
+import { pricePerItem } from "../constants";
+
+const formatCurrency = (currency) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(currency);
+};
+
+const OrderDetails = createContext();
+
+function useOrderDetails() {
+  const context = useContext(OrderDetails);
+
+  if (!context) {
+    throw new Error(
+      "useOrderDetails must be used within an OrderDetailsProvider"
+    );
+  }
+
+  return context;
+}
+
+function calculateSubtotal(optionType, optionCounts) {
+  let optionCount = 0;
+  for (const count of optionCounts[optionType].values()) {
+    optionCount += count;
+  }
+
+  return optionCount * pricePerItem[optionType];
+}
+
+const OrderDetailsProvider = (props) => {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: new Map(),
+    toppings: new Map(),
+  });
+
+  const zeroCurrency = formatCurrency(0);
+  const [totals, setTotals] = useState({
+    scoops: zeroCurrency,
+    toppings: zeroCurrency,
+    grandTotal: zeroCurrency,
+  });
+
+  useEffect(() => {
+    const scoopsSubtotal = calculateSubtotal("scoops", optionCounts);
+    const toppingsSubtotal = calculateSubtotal("toppings", optionCounts);
+    const grandTotal = scoopsSubtotal + toppingsSubtotal;
+
+    setTotals({
+      scoops: formatCurrency(scoopsSubtotal),
+      toppings: formatCurrency(toppingsSubtotal),
+      grandTotal: formatCurrency(grandTotal),
+    });
+  }, [optionCounts]);
+
+  const value = useMemo(() => {
+    const updateItemCount = (itemName, newItemCount, optionType) => {
+      const newOptionCounts = { ...optionCounts };
+      const optionCountsMap = optionCounts[optionType];
+      optionCountsMap.set(itemName, parseInt(newItemCount));
+
+      setOptionCounts(newOptionCounts);
+    };
+
+    //getter: 주문한 스쿱 및 토핑 옵션 세부사항 포함한 객체
+    //setter: updateOptionCount
+    return [{ ...optionCounts, totals }, updateItemCount];
+  }, [optionCounts, totals]);
+
+  return <OrderDetails.Provider {...props} value={value} />;
+};
+
+export { useOrderDetails, OrderDetailsProvider };
---
+++ src/test-utils/testing-library-utils.js
@@ -0,0 +1,11 @@
+//testing library custom render to setup providers.
+import { render } from "@testing-library/react";
+import { OrderDetailsProvider } from "../contexts/OrderDetails";
+
+const renderWithContext = (ui, options) =>
+  render(ui, { wrapper: OrderDetailsProvider, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+//override render method
+export { renderWithContext as render };
